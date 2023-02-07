23f1320b80225651c214e3b6192e4471ccf2e19a
rvishnuvardhann <vrayarakula@gmail.com>
2023-02-07 22:16:47 +0530

    fetch() from ES:6 is used

--- aja.js
+++ aja.js
@@ -1,16 +1,28 @@
 function readData(){
-    let xhttp=new XMLHttpRequest();
-    let div=document.getElementById("Jsondata")
-    xhttp.onreadystatechange=function(){
-        if(this.readyState==4 & this.status==200){
-            // console.log(JSON.parse(xhttp.responseText));
-            let data=JSON.parse(xhttp.responseText);
+    // let xhttp=new XMLHttpRequest();
+    let div1=document.getElementById("usingfetch")
+    // xhttp.onreadystatechange=function(){
+    //     if(this.readyState==4 & this.status==200){
+    //         // console.log(JSON.parse(xhttp.responseText));
+    //         let data=JSON.parse(xhttp.responseText);
+    //         data.forEach(ele=>{
+    //             div.innerHTML+=ele.name+":::"+ele.email+"<br/>";
+    //         })
+    //     }
+    // } 
+    // xhttp.open("GET","https://jsonplaceholder.typicode.com/users",true);
+    // xhttp.send();
+    let response=fetch("https://jsonplaceholder.typicode.com/users")
+    //console.log(response);
+    response.then((res)=>{
+        //console.log(res);
+        res.json().then((data)=>{
+            //console.log(data);
             data.forEach(ele=>{
-                div.innerHTML+=ele.name+":::"+ele.email+"<br/>";
+                div1.innerHTML+=ele.name+":::"+ele.email+"<br/>"
             })
-        }
-    } 
-    xhttp.open("GET","https://jsonplaceholder.typicode.com/users",true);
-    xhttp.send();
+        })
+
+    })
 }
 readData();
